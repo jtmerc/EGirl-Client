be2af288f9c2e6ac9047c83bed3923cc283d9974
jadentm <jadentm@hotmail.com>
2022-07-11 10:05:16 -0500

    tried

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-function App() {
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Chat from "./components/chat/Chat";
+
+import { createSession } from "./actions/watson";
+
+import axios from "axios";
+
+if (localStorage.session) {
+  axios.defaults.headers.common["session_id"] = localStorage.session;
+} else {
+  delete axios.defaults.headers.common["session_id"];
+}
+
+// Connect application
+const App = () => {
+  useEffect(() => {
+    if (!localStorage.session) {
+      store.dispatch(createSession());
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="BG">
+    <Provider store={store}>
+      <div className="container">
+        {/* Insert Chat */}
+        <Chat />
+      </div>
+    </Provider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+// Import dependencies
+import {combineReducers} from "redux";
+
+// Import reducers
+
+import watson from "./watson";
+
+// Export combined reducers
+export default combineReducers({watson});
